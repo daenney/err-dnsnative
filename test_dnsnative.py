aa0e340be0c7d1830e2abc4b25aead908be59efe
Daniele Sluijters <github@daenney.net>
2013-11-28 16:16:08 +0100

    tests: Clean up and reorganise everything.
    * Clean up and reorder the imports.
    * Add extra new lines before if __name__ for legibility.
    * Add a docstring to every test.
    * Split up the tests in its own classes, one per method.
    * Every test we run now has its own method named after what it tests and
      a docstring.

--- test_dnsnative.py
+++ test_dnsnative.py
@@ -1,50 +1,83 @@
-import unittest
 import os
+import unittest
+import logging
 
 import pep8
 from errbot.backends.test import FullStackTest, pushMessage, popMessage
 
 import dnsnative
 
+# Make the test less noisy.
+logging.getLogger('yapsy').setLevel(logging.CRITICAL)
+logging.getLogger('Rocket.Errors').setLevel(logging.CRITICAL)
+logging.getLogger('errbot.plugins.DNSNative').setLevel(logging.CRITICAL)
 
-class DNSNativeUtilitiesTest(unittest.TestCase):
-    def test_is_ip(self):
+
+class DNSNativeIsIPTest(unittest.TestCase):
+    """Test our is_ip() method."""
+
+    def test_is_ip_v4(self):
+        """Test if we can determine the passed value is an IPv4 address.
+        IP address."""
         result = dnsnative.DNSNative.is_ip('10.120.1.1')
         self.assertEqual(result, True)
 
+    def test_is_ip_ipv6(self):
+        """Test if we can determine the passed value is an IPv6 address.
+        IP address."""
         result = dnsnative.DNSNative.is_ip('::1')
         self.assertEqual(result, True)
 
+    def test_is_ip_ipv6(self):
+        """Test if we can determine the passed value isn't an IP address."""
         result = dnsnative.DNSNative.is_ip('abra.cada.bra')
         self.assertEqual(result, False)
 
-    def test_get_host_by_ip(self):
+
+class DNSNativeGetHostByTest(unittest.TestCase):
+    """Test our get_host_by_* methods."""
+
+    def test_get_host_by_ip_v4_localhost(self):
+        """Resolve the 127.0.0.1 to localhost."""
         expected = "127.0.0.1 resolves to localhost"
         result = dnsnative.DNSNative.get_host_by_ip('127.0.0.1')
         self.assertRegex(result, expected)
 
+    def test_get_host_by_ip_v4_unknown(self):
+        """Try to resolve something that's unlikely to have a PTR."""
         expected = "Could not resolve 255.255.255.254."
         result = dnsnative.DNSNative.get_host_by_ip('255.255.255.254')
         self.assertEqual(result, expected)
 
+    def test_get_host_by_ip_v4_google_dns(self):
+        """Resolve a known public IPv4 address."""
         expected = "8.8.4.4 resolves to google-public-dns-b.google.com."
         result = dnsnative.DNSNative.get_host_by_ip('8.8.4.4')
         self.assertEqual(result, expected)
 
+    def test_get_host_by_ip_v6_localhost(self):
+        """Resolve ::1 to localhsot."""
         expected = "::1 resolves to localhost."
         result = dnsnative.DNSNative.get_host_by_ip('::1')
         self.assertEqual(result, expected)
 
+    def test_get_host_by_ip_v6_google_dns(self):
+        """Resolve a know public IPv6 address."""
         expected = ('2001:4860:4860::8844 resolves to google-public-dns-'
                     'b.google.com.')
         result = dnsnative.DNSNative.get_host_by_ip('2001:4860:4860::8844')
         self.assertEqual(result, expected)
 
-    def test_get_host_by_name(self):
+    def test_get_host_by_name_localhost(self):
+        """Resolve localhost to it's IPv4 and IPv6 counterparts."""
+        # Test that localhost resolves to IPv4 and IPv6.
         expected = "localhost resolves to ::1, 127.0.0.1"
         result = dnsnative.DNSNative.get_host_by_name('localhost')
         self.assertRegex(result, expected)
 
+    def test_get_host_by_name_google_dns(self):
+        """Resolve a Google Public DNS hostname to its IPv4 and IPv6
+        counterparts."""
         result = dnsnative.DNSNative.get_host_by_name('google-public-dns-'
                                                       'b.google.com')
         self.assertRegex(result,
@@ -52,28 +85,38 @@ class DNSNativeUtilitiesTest(unittest.TestCase):
         self.assertRegex(result, '8.8.4.4')
         self.assertRegex(result, '2001:4860:4860::8844')
 
+    def test_get_host_by_name_unknown(self):
+        """Try to resolve a bogus hostname."""
         expected = 'Could not resolve abra.cada.bra.'
         result = dnsnative.DNSNative.get_host_by_name('abra.cada.bra')
         self.assertEqual(result, expected)
 
 
 class DNSNativeBotTests(FullStackTest):
+    """Test interaction with the bot. In this case we need a complete bot, send
+    messages to the bot and check the reply."""
+
     def setUp(self):
         me = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
         # Adding /la/bla to path is needed because of the path mangling
-        # FullStackTest does on extra_test_file
+        # FullStackTest does on extra_test_file.
         plugin_dir = os.path.join(me, 'la', 'bla')
+        # Call our parent's setUp() method but pass our directory to
+        # extra_test_file so our plugin is loaded.
         super(DNSNativeBotTests, self).setUp(extra_test_file=plugin_dir)
 
     def test_host(self):
+        """Send host command to the bot, verify we get help back."""
         pushMessage('!host')
         self.assertIn('host: provide a hostname or an IP', popMessage())
 
     def test_host_help(self):
+        """Send host help command, verify we get help back."""
         pushMessage('!host help')
         self.assertIn('host: provide a hostname or an IP', popMessage())
 
     def test_host_hostname(self):
+        """Send host hostname, verify we get the correctly reply."""
         pushMessage('!host google-public-dns-b.google.com')
         response = popMessage()
         self.assertRegex(response,
@@ -82,6 +125,8 @@ class DNSNativeBotTests(FullStackTest):
         self.assertRegex(response, '2001:4860:4860::8844')
 
     def test_host_hostnames(self):
+        """Send host with multiple hostnames, verify we get multiple responses
+        back with the correct content."""
         pushMessage('!host google-public-dns-b.google.com google-public-dns'
                     '-b.google.com')
         response1 = popMessage()
@@ -96,15 +141,20 @@ class DNSNativeBotTests(FullStackTest):
         self.assertRegex(response2, '2001:4860:4860::8844')
 
     def test_host_ip(self):
+        """Send host IP, verify we get the expected hostname back."""
+        # IPv4
         pushMessage('!host 8.8.4.4')
         self.assertEqual('8.8.4.4 resolves to google-public-dns-b.google.com.',
                          popMessage())
+        # IPv6
         pushMessage('!host 2001:4860:4860::8844')
         self.assertEqual(
             '2001:4860:4860::8844 resolves to '
             'google-public-dns-b.google.com.', popMessage())
 
     def test_host_ips(self):
+        """Send host with multiple IP's, verify we get the expected hostnames
+        back."""
         pushMessage('!host 8.8.4.4 2001:4860:4860::8844')
         self.assertEqual('8.8.4.4 resolves to google-public-dns-b.google.com.',
                          popMessage())
@@ -114,6 +164,7 @@ class DNSNativeBotTests(FullStackTest):
 
 
 class TestCodeFormat(unittest.TestCase):
+    """Test suite that validates our code adheres to certain standards."""
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
@@ -121,5 +172,7 @@ class TestCodeFormat(unittest.TestCase):
         result = pep8style.check_files(['dnsnative.py', 'test_dnsnative.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+
 if __name__ == '__main__':
         unittest.main()
